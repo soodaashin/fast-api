29f7f5f3508c47ae06530b3cdf84fb70e6a9eb74
aashin <aashinsood785@gmail.com>
2024-08-26 12:23:59 +0530

    26_august commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -7,3 +8,14 @@ app = FastAPI()
 async def root():
     return {"message": "welcome to fast new fast api"}
 
+
+@app.get("/posts")
+def get_posts():
+    return{"data:" "this is your post"}
+
+
+@app.post("/createposts")
+def createposts(data: dict = Body(...) ):
+    print(data)
+    return {"new post" : f"title {data['title']} content : {data['content']}"}
+ 
