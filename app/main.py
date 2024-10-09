c44c0603b823966661feff6ae9da554848cf2d8e
aashin <aashinsood785@gmail.com>
2024-10-10 00:04:06 +0530

    last commit for today

--- app/main.py
+++ app/main.py
@@ -180,7 +180,7 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 
-@app.get('/users/{id}')
+@app.get('/users/{id}', response_model=schemas.UserOut)
 def get_user(id : int, db: Session = Depends(get_db)):
         user = db.query(models.User).filter(models.User.id == id).first()
         
