7bba4966b480670278be13fd092a2e10a71ebace
aashin <aashinsood785@gmail.com>
2024-10-16 01:17:55 +0530

    16-10-24

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
-from .. import database, schemas, utils
+from .. import database, schemas, utils, oauth2
 from .. import models
-
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 
 router = APIRouter(tags=["Authentication"])
 
 @router.post("/login")
-def login(user_credentials: schemas.Userlogin, db:Session = Depends(database.get_db)):
+def login(user_credentials : OAuth2PasswordRequestForm = Depends(), db:Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
     
     
@@ -22,5 +22,6 @@ def login(user_credentials: schemas.Userlogin, db:Session = Depends(database.get
 
 #create token 
 #return token
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
 
-    return {"token" : "example token"}
+    return {"access_token" : access_token, "token_type": "bearer"}
